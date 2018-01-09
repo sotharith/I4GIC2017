b8b6c0ad64141a10eb387ecdc7fe0478f1c0fc1b
HENG Sotharith <heng.sotharith@gmail.com>
2018-01-09 14:07:53 +0700

    learn javascript

---
+++ JS/hello.js
@@ -0,0 +1,31 @@
+var j = 1;
+
+function displayAlert(params) {
+    //var a = 11;
+    /*
+    text=(a==10)? 'a is 10':'a is not 10';
+    alert('Hello, welcome to JS world' + i);
+    a = (1 == '1');
+    a = (1 === '1');
+    */
+
+    a = 10;
+
+    switch (a) {
+        case 10:
+            
+            break;
+        case 11:
+
+            break;
+        default:
+            break;
+    }
+
+    for(i=0; i<10; i++){
+        if (i == 3) continue;
+        i += 1;
+    }
+}
+
+
